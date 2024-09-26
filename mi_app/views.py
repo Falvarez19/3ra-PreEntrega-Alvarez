e1c0b17f62c0b07da42dc3e247cda4b0bb527409
Alvaro Alvarez <110843582+Falvarez19@users.noreply.github.com>
2024-09-26 15:13:02 -0300

    Ya con template

--- mi_app/views.py
+++ mi_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
-# Create your views here.
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('agregar_producto')
+    else:
+        form = ProductoForm()
+    return render(request, 'agregar_producto.html', {'form': form})
+
+def buscar_producto(request):
+    if 'nombre' in request.GET:
+        nombre = request.GET['nombre']
+        productos = Producto.objects.filter(nombre__icontains=nombre)
+    else:
+        productos = Producto.objects.all()
+    return render(request, 'buscar_producto.html', {'productos': productos})
+
+def inicio(request):
+    return render(request, 'mi_app/inicio.html')
